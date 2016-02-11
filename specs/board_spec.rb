51f89ababf02d9fd613aa05cf78d865c8646f3b7
Raul A. Gonzalez <Nawn1994@gmail.com>
2016-02-10 18:34:12 -0800

    Need to create Tests. Rough Psuedo Code Made

--- specs/board_spec.rb
+++ specs/board_spec.rb
@@ -1,1 +1,19 @@
 require_relative 'spec_helper.rb'
+
+describe Board do
+  before(:each) do
+    @board = Board.new
+  end
+  
+  describe "#new" do
+    it "builds empty board" do
+      expect(@board).to be_a(Board)
+    end
+    
+    context "when freshly built" do
+      it "rows will be an Empty array" do
+        expect(@board.rows).to eql([])
+      end
+    end
+  end
+end
