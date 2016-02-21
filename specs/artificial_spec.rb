ef6b337b7f3dec9427656671aef0e4ab19e1d645
Raul A. Gonzalez <Nawn1994@gmail.com>
2016-02-20 18:45:01 -0800

    Finished tests

--- specs/artificial_spec.rb
+++ specs/artificial_spec.rb
@@ -8,4 +8,22 @@ describe Artificial do
   it "inherits from Player" do
     expect(Artificial).to be < Player
   end
+  
+  describe "#generate_code" do
+    it "returns an Array" do
+      expect(@enemy.generate_code).to be_an(Array)
+    end
+    
+    it "returns 4 colors" do
+      expect(@enemy.generate_code.size).to eql(4)
+    end
+    
+    it "will be random every time" do
+      poop = @enemy.generate_code
+      poop_two = @enemy.generate_code
+      expect(poop).not_to eql(poop_two)
+    end
+  end
+  
+  #need to make the Turns AI
 end
--- specs/row_spec.rb
+++ specs/row_spec.rb
@@ -1,1 +1,34 @@
 require_relative 'spec_helper.rb'
+
+describe Row do
+  before(:each) do
+    @row = Row.new
+  end
+  
+  it "is a Row" do
+    expect(@row).to be_a(Row)
+  end
+  
+  context "when given no input" do
+    it "returns nil on #code" do
+      expect(@row.code).to be_nil
+    end
+    
+    it "returns nil on #result" do
+      expect(@row.result).to be_nil
+    end
+    
+    it "does not have any other attributes" do
+      expect {@row.caca}.to raise_error
+      expect {@row.poop}.to raise_error
+    end
+  end
+  
+  context "when given input" do
+    it "will return Arrays" do
+      poop = Row.new(%w(Bl Br O P), %w(X X x))
+      expect(poop.code).to be_an(Array)
+      expect(poop.result).to be_an(Array)
+    end
+  end
+end
